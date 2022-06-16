06473c264b2aa141498d3c3766b6b926792a8f2c
job <hujunjob@qq.com>
2022-06-16 16:47:31 +0800

    update

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ App = {
     getTokenId:function(){
         var query = window.location.href;
         if(printLog)console.log("getTokenId url="+query)
-        var vars = query.split("/");
+        var vars = query.split("?");
         // for (var i=0;i<vars.length;i++) {
         //         var pair = vars[i].split("=");
         //         if(pair[0] == variable){return pair[1];}
